265bd61b3536c46d01f193d8ed2c9ff51356656e
EmmaFC <emmafecor@hotmail.com>
2022-01-07 20:45:20 +0100

    changes

--- src/ejercicio-02.js
+++ src/ejercicio-02.js
@@ -1,3 +1,3 @@
 // EJERCICIO-02:
 
-export let sumar = (a,b) => document.getElementById('ejercicio-02-solucion').innerHTML = (a + b);
+export let sumar = (a,b) => document.getElementById('ejercicio-02-solucion').innerHTML = `La suma de ${a} y ${b} es ${a +b}`;
--- src/ejercicio-14.js
+++ src/ejercicio-14.js
@@ -4,7 +4,7 @@ export let numberDividers = (num_01) => {
     for(let i = 0; i<10; i++){
         if (num_01 % i == 0) {
             let divider = document.createElement('p');
-            divider.innerHTML = `X El número ${num_01} es divisible por ${i}`;
+            divider.innerHTML = `El número ${num_01} es divisible por ${i}`;
             document.getElementById('ejercicio-14-solucion').appendChild (divider);
         }
         `El número ${num_01} no es divisible por ${i}`
