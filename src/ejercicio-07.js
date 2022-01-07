0ccd7889e75f64c06e821b6c6ff72464c6c6fb1a
EmmaFC <emmafecor@hotmail.com>
2022-01-07 21:08:53 +0100

    changes

--- src/ejercicio-07.js
+++ src/ejercicio-07.js
@@ -3,6 +3,7 @@
 export let numberDivisibleBy2 = (num_01) => {
     if (num_01 % 2 == 0){
         document.getElementById('ejercicio-07-solucion').innerHTML = `Ejercicio 07: ${num_01} es divisible por 2`;
+        return;
     } 
     document.getElementById('ejercicio-07-solucion').innerHTML = `Ejercicio 07: ${num_01} no es divisible por 2`;
 }
